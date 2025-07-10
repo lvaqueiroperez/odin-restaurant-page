5fa4c51e85cca53ee16bb7295e860bf46453fe0f
Luis <43774737+lvaqueiroperez@users.noreply.github.com>
2025-07-10 12:04:40 +0200

    Add button event listeners basic skeleton

--- src/index.js
+++ src/index.js
@@ -3,3 +3,21 @@ import { loadHomeContent } from "./home.js";
 
 console.log("aaaa");
 loadHomeContent();
+
+document.querySelector("ul").addEventListener("click", (e) => {
+
+    switch (e.target.id) {
+        case "home":
+
+            break;
+
+        case "menu":
+
+            break;
+
+        case "about":
+
+            break;
+    }
+
+});
