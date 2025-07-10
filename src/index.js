b50f00247d47767517bc050c2cae16c5e0d43e7b
Luis <43774737+lvaqueiroperez@users.noreply.github.com>
2025-07-10 12:53:42 +0200

    Implement module load logic with event listeners Also, created a removeContent function

--- src/index.js
+++ src/index.js
@@ -1,18 +1,27 @@
 import "./styles.css";
 import { loadHomeContent } from "./home.js";
+import { loadMenuContent } from "./menu.js";
 
 console.log("aaaa");
 loadHomeContent();
+
+function removeContent() {
+    Array.from(document.querySelector("#content").children).forEach((child) => {
+        child.remove();
+    });
+}
 
 document.querySelector("ul").addEventListener("click", (e) => {
 
     switch (e.target.id) {
         case "home":
-
+            removeContent();
+            loadHomeContent();
             break;
 
         case "menu":
-
+            removeContent();
+            loadMenuContent();
             break;
 
         case "about":
